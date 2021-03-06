7166f7a7e9c3043babd7b70e2206b65ca6fb829b
Agnieszka Platek <aga@platki.waw.pl>
2021-03-06 18:42:55 +0100

    Add user profile and delete

--- src/views/users/UserProfile.vue
+++ src/views/users/UserProfile.vue
@@ -1,55 +1,69 @@
 <template>
-  <section class="my-4 mx-3">
-    <div class="media ml-2">
-      <div class="media-body">
-        <h2 class="account-heading">{{ user.username }}</h2>
-        <p class="text-secondary">{{ user.email }}</p>
-
-        <p v-if="num_sets === 1" class="h5"><b>1</b> set</p>
-        <p v-else class="h5">
-          <b>{{ num_sets }}</b> sets
-        </p>
-        <p v-if="num_flashcards === 1" class="h5"><b>1</b> flashcard</p>
-        <p v-else class="h5">
-          <b>{{ num_flashcards }}</b> flashcards
-        </p>
-      </div>
+  <section>
+    <div class="text-center mt-3">
+      <h2>{{ username }}</h2>
+      <h6 class="mb-4">{{ email }}</h6>
+      <p v-if="num_sets === 1" class="h5"><b>1</b> set</p>
+      <p v-else class="h5">
+        <b>{{ num_sets }}</b> sets
+      </p>
+      <p v-if="num_flashcards === 1" class="h5"><b>1</b> flashcard</p>
+      <p v-else class="h5">
+        <b>{{ num_flashcards }}</b> flashcards
+      </p>
     </div>
-    {% if not demo %}
-    <form method="POST" enctype="multipart/form-data">
-      <br />
-      <fieldset class="form-group">
-        <legend class="border-bottom mb-4">
-          {% translate 'Profile Info' %}
-        </legend>
-      </fieldset>
-
-      <div class="fluid-container">
-        <button
-          class="btn btn-square btn-primary col-md-4 d-inline-block mb-4"
-          type="submit"
-        >
-          {% translate 'Update' %}
+    <!-- 
+    <form @submit="updateUserData">
+      <label>Username:</label>
+      <input type="username" required v-model="username" />
+      <label>Email:</label>
+      <input type="email" required v-model="email" />
+          <div class="submit mt-5">
+        <button class="btn btn-primary btn-block px-5" type="submit">
+          Update
         </button>
-        <div class="row col-12">
-          <div class="col-4 offset-4 col-lg-5 offset-lg-5"></div>
-          <form method="POST">
-            <a
-              href=""
-              class="btn btn-square btn-sm btn-delete col-4 col-lg-2 d-inline-block"
-            >
-              Delete account</a
-            >
-          </form>
-        </div>
       </div>
-    </form>
+    </form> -->
+    <div class="text-right mt-5">
+      <button class="btn btn-sm btn-delete col-4 col-lg-2 d-inline-block">
+        Delete account
+      </button>
+    </div>
   </section>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "UserProfile",
+  data() {
+    return {
+      username: "",
+      email: "",
+      num_sets: "",
+      num_flashcards: "",
+    };
+  },
+  mounted() {
+    axios
+      .get("auth/users/me", {
+        headers: {
+          Authorization: "Token 4dcdca18cc571489b5840d2041ed8b36588e0e33",
+        },
+      })
+      .then(
+        (response) => (
+          (this.username = response.data["username"]),
+          (this.email = response.data["email"]),
+          (this.num_sets = response.data["num_sets"]),
+          (this.num_flashcards = response.data["num_flashcards"])
+        )
+      )
+      .catch((error) => console.log(error));
+  },
+  // methods: {
+  //   updateUserData() {},
+  // },
 };
 </script>
 
--- src/views/users/UserProfileDelete.vue
+++ src/views/users/UserProfileDelete.vue
@@ -1,39 +1,43 @@
 <template>
-  <section class="mt-4">
-    <form method="POST">
-      <fieldset class="form-group mt-2 mx-2">
-        <legend class="border-bottom mb-3 ml-3">
-          Are you sure you want to delete your account?
-        </legend>
-        <p class="ml-3">
-          This will permanently delete your profile and any content you have
-          generated.
-        </p>
-      </fieldset>
-      <div class="row mb-4 ml-3">
-        <button
-          class="btn btn-square btn-delete ml-2 px-4"
-          type="submit"
-          name="delete"
-        >
-          Yes, Delete
-        </button>
-        <a
-          href=""
-          class="btn btn-square btn-back ml-2"
-          role="button"
-          aria-pressed="true"
-        >
-          Cancel</a
-        >
-      </div>
-    </form>
+  <section class="mt-4 mx-1">
+    <div class="warning">
+      <h1 class="h3">Are you sure you want to delete your account?</h1>
+      <h5>
+        This will permanently delete your profile and any content you have
+        generated.
+      </h5>
+    </div>
+    <a href="" @click="accountDelete" class="btn btn-delete px-5" type="submit">
+      Yes, Delete
+    </a>
+    <router-link
+      :to="{ name: 'SetList' }"
+      class="btn btn-back ml-3"
+      role="button"
+      aria-pressed="true"
+    >
+      Cancel</router-link
+    >
   </section>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "UserProfileDelete",
+  methods: {
+    accountDelete() {
+      axios({
+        method: "delete",
+        url: "auth/users/me",
+        headers: {
+          Authorization: "Token 3ba62b1f6855e93af81af6fe28ccd388a7643874",
+        },
+      }).catch((err) => {
+        console.log("error in request", err);
+      });
+    },
+  },
 };
 </script>
 
