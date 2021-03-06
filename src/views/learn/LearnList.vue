133be4cae924b0c2092e90f8d0ebb9b0b5158185
Agnieszka Platek <aga@platki.waw.pl>
2021-03-06 16:54:17 +0100

    Add learning all flashcards and not showing sets with 0 flashcards

--- src/views/learn/LearnList.vue
+++ src/views/learn/LearnList.vue
@@ -5,17 +5,31 @@
         <h1 class="h2">Learn</h1>
       </div>
       <div class="mt-4 mb-3 text-center">
-        <a
+        <router-link
+          :to="{
+            name: 'LearnSession',
+            params: {
+              cards: JSON.stringify(flashcards),
+            },
+          }"
           class="btn btn-learn btn-lg btn-block content-center text-uppercase py-2 mx-auto"
-          href=""
         >
           Learn all flashcards
           <span class="badge badge-light ml-2">{{ total_flashcards }}</span>
-        </a>
+        </router-link>
+        <!-- <router-link
+          :to="{
+            name: 'LearnAll',
+          }"
+          class="btn btn-learn btn-lg btn-block content-center text-uppercase py-2 mx-auto"
+        >
+          Learn all flashcards
+          <span class="badge badge-light ml-2">{{ total_flashcards }}</span>
+        </router-link> -->
       </div>
       <div class="fluid-container">
         <div v-for="set in big_sets" :key="set.id">
-          <div class="btn-group col-12 text-center">
+          <div class="btn-block text-center">
             <button
               type="button"
               class="btn btn-grey btn-block btn-lg dropdown-toggle mb-1"
@@ -27,11 +41,26 @@
               }}</span>
             </button>
             <div class="dropdown-menu">
-              <a class="dropdown-item text-center py-1" href="">
-                Learn all flashcards from the set</a
+              <router-link
+                :to="{
+                  name: 'LearnSession',
+                  params: {
+                    setname: set.name,
+                    cards: JSON.stringify(set.flashcards),
+                  },
+                }"
+                class="dropdown-item text-center py-1"
               >
-              <a class="dropdown-item text-center mb-1 py-1" href="">
-                Choose part to learn</a
+                Learn all flashcards from the set</router-link
+              >
+              <router-link
+                :to="{
+                  name: 'FlashcardFilter',
+                  params: { id: set.id, setname: set.name },
+                }"
+                class="dropdown-item text-center mb-1 py-1"
+              >
+                Choose part to learn</router-link
               >
             </div>
           </div>
@@ -39,24 +68,20 @@
       </div>
       <div class="fluid-container">
         <div v-for="set in small_sets" :key="set.id">
-          <div class="mx-3">
-            <router-link
-              :to="{
-                name: 'LearnSession',
-                params: {
-                  setname: set.name,
-                  cards: JSON.stringify(set.flashcards),
-                },
-              }"
-              class="btn btn-grey btn-lg btn-block mb-1"
-              role="button"
-              aria-pressed="true"
-              >{{ set.name }}
-              <span class="badge badge-primary ml-2">{{
-                set.num_flashcards
-              }}</span>
-            </router-link>
-          </div>
+          <router-link
+            :to="{
+              name: 'LearnSession',
+              params: {
+                setname: set.name,
+                cards: JSON.stringify(set.flashcards),
+              },
+            }"
+            class="btn btn-grey btn-lg btn-block mb-1"
+            >{{ set.name }}
+            <span class="badge badge-primary ml-2">{{
+              set.num_flashcards
+            }}</span>
+          </router-link>
         </div>
       </div>
     </div>
@@ -94,13 +119,16 @@ export default {
         )
       );
     axios
-      .get("auth/users/me", {
+      .get("flashcards/", {
         headers: {
           Authorization: "Token 4dcdca18cc571489b5840d2041ed8b36588e0e33",
         },
       })
       .then(
-        (response) => (this.total_flashcards = response.data["num_flashcards"])
+        (response) => (
+          (this.flashcards = response.data),
+          (this.total_flashcards = response.data.length)
+        )
       );
   },
   computed: {
@@ -108,7 +136,9 @@ export default {
       return this.sets.filter((set) => set.num_flashcards > 20);
     },
     small_sets() {
-      return this.sets.filter((set) => set.num_flashcards <= 20);
+      return this.sets.filter(
+        (set) => set.num_flashcards <= 20 && set.num_flashcards > 0
+      );
     },
   },
 };
